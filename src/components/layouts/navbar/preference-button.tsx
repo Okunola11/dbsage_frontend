f05ec90e7ca8349706f11a0e25fddc89d7fcd90d
okunola <abdayoade@gmail.com>
2024-09-21 23:28:21 +0100

    feature: add NavBar component

---
+++ src/components/layouts/navbar/preference-button.tsx
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react";
+import { Button } from "@/components/ui/button";
+import { SunIcon, MoonIcon } from "lucide-react";
+
+export default function PreferenceButton() {
+  const [isDarkMode, setIsDarkMode] = useState(true);
+
+  useEffect(() => {
+    if (isDarkMode) {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, [isDarkMode]);
+
+  const toggleDarkMode = () => {
+    setIsDarkMode(!isDarkMode);
+  };
+
+  return (
+    <Button
+      variant={"ghost"}
+      size={"icon"}
+      className="dark:text-gray-200"
+      onClick={toggleDarkMode}
+    >
+      {isDarkMode ? (
+        <SunIcon width={20} height={20} />
+      ) : (
+        <MoonIcon width={20} height={20} />
+      )}
+    </Button>
+  );
+}
