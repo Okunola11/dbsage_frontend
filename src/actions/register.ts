0ed4b307d636dc691fdcd960cd2ece4264302e8a
okunola <abdayoade@gmail.com>
2024-09-21 23:02:39 +0100

    chore: add register user action
    This send the request body to the api endpoint for processing.

---
+++ src/actions/register.ts
@@ -0,0 +1,49 @@
+"use server";
+
+import axios from "axios";
+import * as z from "zod";
+
+import { RegisterSchema } from "@/schemas";
+import { apiUrl } from "@/utils/settings.env";
+import { ApiResponse } from "@/types";
+
+export const registerUser = async (
+  values: z.infer<typeof RegisterSchema>
+): Promise<ApiResponse> => {
+  const validatedFields = RegisterSchema.safeParse(values);
+
+  if (!validatedFields.success) {
+    return {
+      status_code: 401,
+      message: "Something went wrong",
+    };
+  }
+
+  try {
+    const response = await axios.post(
+      `${apiUrl}/api/v1/auth/register`,
+      validatedFields.data
+    );
+
+    return {
+      status: response.data.status,
+      status_code: response.data.status_code,
+      message: response.data.message,
+      data: response.data.data,
+    };
+  } catch (error) {
+    if (axios.isAxiosError(error) && error.response) {
+      return {
+        status_code: error.response.status ?? error.response.data.status_code,
+        message: error.response.data.message
+          ? error.response.data.message
+          : "Something went wrong.",
+      };
+    } else {
+      return {
+        status_code: 500,
+        message: "An unexpected error occured.",
+      };
+    }
+  }
+};
