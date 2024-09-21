0db3a24e31b0e610eee68caadf2919494aca84d2
okunola <abdayoade@gmail.com>
2024-09-21 23:07:58 +0100

    chore: put routes into categories
    This will be used for middleware configuration.

---
+++ src/lib/routes.ts
@@ -0,0 +1,16 @@
+export const publicRoutes = ["/", "about-us", "contact-us"];
+
+export const authRoutes = [
+  "/register",
+  "/login",
+  "/forgot-password",
+  "/reset-password",
+];
+
+export const superAdminRoutes = ["dashboard/admin/faqs"];
+
+export const apiAuthPrefix = "/api/";
+
+export const DEFAULT_LOGIN_REDIRECT = "/dashboard";
+
+export const clientRoutes = ["/dashboard", "/history", "/profile"];
