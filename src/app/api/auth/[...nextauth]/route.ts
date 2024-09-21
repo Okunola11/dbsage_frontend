e2402c158fa57965fee564277a56d6da13fbb6f1
okunola <abdayoade@gmail.com>
2024-09-21 23:12:22 +0100

    chore: init next-auth

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,9 @@
+import NextAuth from "next-auth/next";
+
+import authConfig from "@/config/auth.config";
+
+const handler = NextAuth({
+  ...authConfig,
+});
+
+export { handler as GET, handler as POST };
