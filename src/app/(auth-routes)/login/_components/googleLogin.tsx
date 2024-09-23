37ce9c20a8d9e88b12fe4ee668378dabd3eaa19d
okunola <abdayoade@gmail.com>
2024-09-24 00:02:23 +0100

    chore: updated props typescript type
    I update the GoogleLogin props type to their actual type instead of
    using any.

--- src/app/(auth-routes)/login/_components/googleLogin.tsx
+++ src/app/(auth-routes)/login/_components/googleLogin.tsx
@@ -2,12 +2,13 @@ import { signIn } from "next-auth/react";
 
 import { googleIcon } from "./icon";
 import CustomButton from "@/components/common/button/commonButton";
+import { Translator } from "@/types";
 
-export type Translation = {
-  t: any;
+export type LoginProps = {
+  t: Translator;
 };
 
-const GoogleLogin = ({ t }: Translation) => {
+const GoogleLogin: React.FC<LoginProps> = ({ t }) => {
   return (
     <div className="flex flex-col justify-center space-y-4 sm:flex-row sm:space-x-6 sm:space-y-0">
       <CustomButton
