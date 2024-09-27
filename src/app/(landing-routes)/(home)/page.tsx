d542ce330f1d896020f35b7df98b58dd70b12e76
okunola <abdayoade@gmail.com>
2024-09-27 10:30:11 +0100

    feat: added landing page

---
+++ src/app/(landing-routes)/(home)/page.tsx
@@ -0,0 +1,11 @@
+import { Hero } from "@/components/layouts/hero/Hero";
+import HowItWorks from "./_components/layout/howItWorks";
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <HowItWorks />
+    </>
+  );
+}
