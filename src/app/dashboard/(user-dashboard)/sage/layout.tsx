81a8d84d6c8c41d99e4e600d4d3b7899408b5465
okunola <abdayoade@gmail.com>
2024-10-06 15:33:53 +0100

    chore: added DatabaseProvider
    It provides properties of the DatabaseContext to all the children of the
    layout. They will be used to manage database tables state in the
    application.

--- src/app/dashboard/(user-dashboard)/sage/layout.tsx
+++ src/app/dashboard/(user-dashboard)/sage/layout.tsx
@@ -1,5 +1,6 @@
 import Navbar from "@/components/layouts/navbar";
 import SettingsSidebar from "./_components/settingsSidebar";
+import { DatabaseProvider } from "@/context/databaseContext";
 
 export default async function GeneralLayout({
   children,
@@ -7,7 +8,7 @@ export default async function GeneralLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <>
+    <DatabaseProvider>
       <Navbar />
       <div className="relative mx-auto w-full max-w-[90%] max-lg:overflow-hidden xl:px-4">
         <div className="flex flex-col md:flex-row">
@@ -17,6 +18,6 @@ export default async function GeneralLayout({
           <div className="flex-1 md:w-[calc(100%_-_50px)]">{children}</div>
         </div>
       </div>
-    </>
+    </DatabaseProvider>
   );
 }
