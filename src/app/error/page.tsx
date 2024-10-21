0b8c7873fac8ecd0548a68de09036979e1209ac3
okunola <abdayoade@gmail.com>
2024-10-21 14:34:07 +0100

    feat: implement error page for NextAuth authentication failures
    Display customized error messages to users during authentication process

---
+++ src/app/error/page.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import { useSearchParams, useRouter } from "next/navigation";
+
+enum ErrorType {
+  Configuration = "Configuration",
+  AccessDenied = "AccessDenied",
+  CredentialsSignin = "CredentialsSignin",
+}
+
+export const errorMap: Record<string, string> = {
+  [ErrorType.Configuration]:
+    "There was a problem when trying to authenticate. Please contact us if this error persists.",
+  [ErrorType.AccessDenied]: "Access was denied.",
+  [ErrorType.CredentialsSignin]: "Invalid credentials.",
+};
+
+export default function AuthErrorPage() {
+  const search = useSearchParams();
+  const router = useRouter();
+  const error = search.get("error");
+
+  // Handle the redirect to login with preserved error
+  // const handleReturn = () => {
+  //   router.push(`/login?authError=${error}`);
+  // };
+
+  return (
+    <div className="flex h-screen w-full flex-col items-center justify-center">
+      <button
+        // onClick={handleReturn}
+        className="block max-w-sm rounded-lg border border-gray-200 bg-white p-6 text-center shadow hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700"
+      >
+        <h5 className="mb-2 flex flex-row items-center justify-center gap-2 text-xl font-bold tracking-tight text-gray-900 dark:text-white">
+          Something went wrong
+        </h5>
+        <div className="font-normal text-gray-700 dark:text-gray-400">
+          {error
+            ? errorMap[error]
+            : "Please contact us if this error persists."}
+        </div>
+      </button>
+    </div>
+  );
+}
