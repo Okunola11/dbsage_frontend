feaf00b93b7fd133fcc6d52093c507c8816af5cd
okunola <abdayoade@gmail.com>
2024-10-30 07:40:32 +0100

    refactor: simplify dialog management
    Defined handleDialogOPen and handleDialogClose functions. handleDialogClose is called after the form submission or after logout of the connection. handleDialogOpen is used by the custom button which is now the trigger for the dialog upon click action.

--- src/components/common/dialog/databaseDialog.tsx
+++ src/components/common/dialog/databaseDialog.tsx
@@ -21,10 +21,8 @@ import {
   Dialog,
   DialogContent,
   DialogDescription,
-  DialogFooter,
   DialogHeader,
   DialogTitle,
-  DialogTrigger,
 } from ".";
 import {
   Form,
@@ -37,10 +35,19 @@ import {
 
 const DatabaseDialog = () => {
   const t = useTranslations("dbSage.database");
+  const [isOpen, setIsOpen] = useState(false);
   const [isLoading, startTransition] = useTransition();
   const [activeAction, setActiveAction] = useState("");
   const { updateTables, clearTables } = useDatabaseContext();
   const { toast } = useToast();
+
+  const handleDialogOpen = () => {
+    setIsOpen(true);
+  };
+
+  const handleDialogClose = () => {
+    setIsOpen(false);
+  };
 
   const form = useForm<z.infer<typeof dbConfigSchema>>({
     resolver: zodResolver(dbConfigSchema),
@@ -72,6 +79,7 @@ const DatabaseDialog = () => {
         }
       });
       setActiveAction("");
+      handleDialogClose();
     });
   };
 
@@ -94,6 +102,7 @@ const DatabaseDialog = () => {
         }
       });
       setActiveAction("");
+      handleDialogClose();
     });
   };
 
@@ -101,190 +110,194 @@ const DatabaseDialog = () => {
   const isLoggingOut = isLoading && activeAction === "logout";
 
   return (
-    <Dialog>
-      <DialogTrigger asChild>
-        <CustomButton
-          icon={<BsDatabaseFillLock />}
-          isLeftIconVisible={true}
-          variant={"ghost"}
-          className="p-2 text-sm md:text-base"
-          iconClassName="w-[24px] h-[24px] md:w-[30px] md:h-[30px] text-primary"
-        >
-          {t("connect")}
-        </CustomButton>
-      </DialogTrigger>
+    <>
+      <CustomButton
+        icon={<BsDatabaseFillLock />}
+        isLeftIconVisible={true}
+        variant={"ghost"}
+        className="p-2 text-sm md:text-base"
+        iconClassName="w-[24px] h-[24px] md:w-[30px] md:h-[30px] text-primary"
+        onClick={handleDialogOpen}
+      >
+        {t("connect")}
+      </CustomButton>
 
-      <DialogContent className="max-w-2xl overflow-y-auto max-h-screen md:max-h-[90%]">
-        <DialogHeader>
-          <DialogTitle className="flex gap-2 items-center">
-            <BsDatabaseFillLock className="w-7 h-7" /> {t("title")}
-          </DialogTitle>
-          <DialogDescription>{t("description")}</DialogDescription>
-        </DialogHeader>
+      <Dialog open={isOpen} onOpenChange={handleDialogClose}>
+        <DialogContent className="max-w-2xl overflow-y-auto max-h-screen md:max-h-[90%]">
+          <DialogHeader>
+            <DialogTitle className="flex gap-2 items-center">
+              <BsDatabaseFillLock className="w-7 h-7" /> {t("title")}
+            </DialogTitle>
+            <DialogDescription>{t("description")}</DialogDescription>
+          </DialogHeader>
 
-        <div className="grid">
-          <Form {...form}>
-            <form
-              onSubmit={form.handleSubmit(onSubmit)}
-              className="space-y-6 min-w-[280px] md:min-w-[500px]"
-            >
-              <FormField
-                control={form.control}
-                name="host"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel className="text-neutral-dark-2">
-                      {t("host.label")}
-                    </FormLabel>
-                    <FormControl>
-                      <Input
-                        placeholder={t("host.placeholder")}
-                        {...field}
-                        className={cn(
-                          "w-full rounded-md border px-3 py-5 text-sm leading-[21.78px] transition duration-150 ease-in-out focus:outline-none",
-                          form.formState.errors.host && "border-destructive"
-                        )}
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-
-              <FormField
-                control={form.control}
-                name="port"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel className="text-neutral-dark-2">
-                      {t("port.label")}
-                    </FormLabel>
-                    <FormControl>
-                      <Input
-                        placeholder={t("port.placeholder")}
-                        {...field}
-                        className={cn(
-                          "w-full rounded-md border px-3 py-5 text-sm leading-[21.78px] transition duration-150 ease-in-out focus:outline-none",
-                          form.formState.errors.port && "border-destructive"
-                        )}
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-
-              <FormField
-                control={form.control}
-                name="username"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel className="text-neutral-dark-2">
-                      {t("username.label")}
-                    </FormLabel>
-                    <FormControl>
-                      <Input
-                        placeholder={t("username.placeholder")}
-                        {...field}
-                        className={cn(
-                          "w-full rounded-md border px-3 py-5 text-sm leading-[21.78px] transition duration-150 ease-in-out focus:outline-none",
-                          form.formState.errors.username && "border-destructive"
-                        )}
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-
-              <FormField
-                control={form.control}
-                name="password"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel className="text-neutral-dark-2">
-                      {t("password.label")}
-                    </FormLabel>
-                    <FormControl>
-                      <Input
-                        type="password"
-                        placeholder={t("password.placeholder")}
-                        {...field}
-                        className={cn(
-                          "w-full rounded-md border px-3 py-5 text-sm leading-[21.78px] transition duration-150 ease-in-out focus:outline-none",
-                          form.formState.errors.password && "border-destructive"
-                        )}
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-
-              <FormField
-                control={form.control}
-                name="database"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel className="text-neutral-dark-2">
-                      {t("database.label")}
-                    </FormLabel>
-                    <FormControl>
-                      <Input
-                        placeholder={t("database.placeholder")}
-                        {...field}
-                        className={cn(
-                          "w-full rounded-md border px-3 py-5 text-sm leading-[21.78px] transition duration-150 ease-in-out focus:outline-none",
-                          form.formState.errors.database && "border-destructive"
-                        )}
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-
-              <div className="flex justify-between">
-                <CustomButton
-                  type="button"
-                  variant="primary"
-                  size="default"
-                  className="py-3"
-                  isDisabled={isLoading}
-                  onClick={onLogout}
-                >
-                  {isLoggingOut ? (
-                    <span className="flex items-center gap-x-2">
-                      <span className="animate-pulse">Logging out...</span>{" "}
-                      <LoadingSpinner className="size-4 animate-spin sm:size-5" />
-                    </span>
-                  ) : (
-                    <span>{t("logout")}</span>
+          <div className="grid">
+            <Form {...form}>
+              <form
+                onSubmit={form.handleSubmit(onSubmit)}
+                className="space-y-6 min-w-[280px] md:min-w-[500px]"
+              >
+                <FormField
+                  control={form.control}
+                  name="host"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel className="text-neutral-dark-2">
+                        {t("host.label")}
+                      </FormLabel>
+                      <FormControl>
+                        <Input
+                          placeholder={t("host.placeholder")}
+                          {...field}
+                          className={cn(
+                            "w-full rounded-md border px-3 py-5 text-sm leading-[21.78px] transition duration-150 ease-in-out focus:outline-none",
+                            form.formState.errors.host && "border-destructive"
+                          )}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
                   )}
-                </CustomButton>
+                />
 
-                <CustomButton
-                  type="submit"
-                  variant="primary"
-                  size="default"
-                  className="py-3"
-                  isDisabled={isLoading}
-                >
-                  {isSubmitting ? (
-                    <span className="flex items-center gap-x-2">
-                      <span className="animate-pulse">Connecting...</span>{" "}
-                      <LoadingSpinner className="size-4 animate-spin sm:size-5" />
-                    </span>
-                  ) : (
-                    <span>{t("connect")}</span>
+                <FormField
+                  control={form.control}
+                  name="port"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel className="text-neutral-dark-2">
+                        {t("port.label")}
+                      </FormLabel>
+                      <FormControl>
+                        <Input
+                          placeholder={t("port.placeholder")}
+                          {...field}
+                          className={cn(
+                            "w-full rounded-md border px-3 py-5 text-sm leading-[21.78px] transition duration-150 ease-in-out focus:outline-none",
+                            form.formState.errors.port && "border-destructive"
+                          )}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
                   )}
-                </CustomButton>
-              </div>
-            </form>
-          </Form>
-        </div>
-      </DialogContent>
-    </Dialog>
+                />
+
+                <FormField
+                  control={form.control}
+                  name="username"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel className="text-neutral-dark-2">
+                        {t("username.label")}
+                      </FormLabel>
+                      <FormControl>
+                        <Input
+                          placeholder={t("username.placeholder")}
+                          {...field}
+                          className={cn(
+                            "w-full rounded-md border px-3 py-5 text-sm leading-[21.78px] transition duration-150 ease-in-out focus:outline-none",
+                            form.formState.errors.username &&
+                              "border-destructive"
+                          )}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+
+                <FormField
+                  control={form.control}
+                  name="password"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel className="text-neutral-dark-2">
+                        {t("password.label")}
+                      </FormLabel>
+                      <FormControl>
+                        <Input
+                          type="password"
+                          placeholder={t("password.placeholder")}
+                          {...field}
+                          className={cn(
+                            "w-full rounded-md border px-3 py-5 text-sm leading-[21.78px] transition duration-150 ease-in-out focus:outline-none",
+                            form.formState.errors.password &&
+                              "border-destructive"
+                          )}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+
+                <FormField
+                  control={form.control}
+                  name="database"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel className="text-neutral-dark-2">
+                        {t("database.label")}
+                      </FormLabel>
+                      <FormControl>
+                        <Input
+                          placeholder={t("database.placeholder")}
+                          {...field}
+                          className={cn(
+                            "w-full rounded-md border px-3 py-5 text-sm leading-[21.78px] transition duration-150 ease-in-out focus:outline-none",
+                            form.formState.errors.database &&
+                              "border-destructive"
+                          )}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+
+                <div className="flex justify-between">
+                  <CustomButton
+                    type="button"
+                    variant="primary"
+                    size="default"
+                    className="py-3"
+                    isDisabled={isLoading}
+                    onClick={onLogout}
+                  >
+                    {isLoggingOut ? (
+                      <span className="flex items-center gap-x-2">
+                        <span className="animate-pulse">Logging out...</span>{" "}
+                        <LoadingSpinner className="size-4 animate-spin sm:size-5" />
+                      </span>
+                    ) : (
+                      <span>{t("logout")}</span>
+                    )}
+                  </CustomButton>
+
+                  <CustomButton
+                    type="submit"
+                    variant="primary"
+                    size="default"
+                    className="py-3"
+                    isDisabled={isLoading}
+                  >
+                    {isSubmitting ? (
+                      <span className="flex items-center gap-x-2">
+                        <span className="animate-pulse">Connecting...</span>{" "}
+                        <LoadingSpinner className="size-4 animate-spin sm:size-5" />
+                      </span>
+                    ) : (
+                      <span>{t("connect")}</span>
+                    )}
+                  </CustomButton>
+                </div>
+              </form>
+            </Form>
+          </div>
+        </DialogContent>
+      </Dialog>
+    </>
   );
 };
 
