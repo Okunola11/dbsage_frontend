f9cd7a1b6decb17ef954e7d04795ceaac98b907d
okunola <abdayoade@gmail.com>
2024-11-06 10:27:43 +0100

    feat: add development page

---
+++ src/app/development/page.tsx
@@ -0,0 +1,13 @@
+import UnderDevelopment from "@/components/common/underDevelopment";
+import Navbar from "@/components/layouts/navbar";
+
+const Development = () => {
+  return (
+    <>
+      <Navbar />
+      <UnderDevelopment />
+    </>
+  );
+};
+
+export default Development;
