cf6e3b299630f397c8a25155903e652396b30174
okunola <abdayoade@gmail.com>
2024-11-06 06:50:08 +0100

    refactor: migrate DisplayLottie to React Functional Component
    Update DisplayLottie to:
    - Become a React Functional Component
    - Import Lottie as dynamic component with SSR disabled
    - Set default loading state to LoadingSpinner component
    - Improve compatibility and performance

--- src/components/miscellaneous/displayLottie.tsx
+++ src/components/miscellaneous/displayLottie.tsx
@@ -1,29 +1,31 @@
 "use client";
 
-import { Component, ReactNode, Suspense } from "react";
-import Lottie from "lottie-react";
+import { FC } from "react";
+import dynamic from "next/dynamic";
 import LoadingSpinner from "./loadingSpinner";
+
+const Lottie = dynamic(() => import("lottie-react"), {
+  ssr: false,
+  loading: () => <LoadingSpinner />,
+});
 
 interface DisplayLottieProps {
   animationData: unknown;
 }
 
-export default class DisplayLottie extends Component<DisplayLottieProps> {
-  render(): ReactNode {
-    const { animationData } = this.props;
-    const defaultOptions = {
-      loop: true,
-      autoplay: true,
-      animationData: animationData,
-    };
+const DisplayLottie: FC<DisplayLottieProps> = ({ animationData }) => {
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animationData,
+  };
 
-    return (
-      <Suspense fallback={<LoadingSpinner />}>
-        <Lottie
-          animationData={defaultOptions.animationData}
-          loop={defaultOptions.loop}
-        />
-      </Suspense>
-    );
-  }
-}
+  return (
+    <Lottie
+      animationData={defaultOptions.animationData}
+      loop={defaultOptions.loop}
+    />
+  );
+};
+
+export default DisplayLottie;
